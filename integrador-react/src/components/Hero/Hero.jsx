4ac61081296f0f8eadcdae7fa026bde19013b2e8
Laureano <laureanojuarez69@gmail.com>
2025-07-15 15:34:38 -0300

    Refactor Hero component structure for improved layout and styling

--- integrador-react/src/components/Hero/Hero.jsx
+++ integrador-react/src/components/Hero/Hero.jsx
@@ -3,15 +3,24 @@ import territory from "/images/marcas/ford/territory.png";
 
 export function Hero() {
   return (
-    <div className="flex flex-col items-center bg-blue-200">
-      <div className="">
-        <p className="w-1/2 text-2xl">Compra Online</p>
-        <h1 className="text-3xl w-1/2">Consegui tu proximo 0km hoy!</h1>
+    <section className="flex flex-col items-center bg-blue-200 mt-7 p-4">
+      <div className="w-full text-center justify-center items-">
+        <p className="text-2xl">Compra Online</p>
+        <h1 className="text-3xl">Consegui tu proximo 0km hoy!</h1>
       </div>
-      <div className="">
-        <img src={Tracker} alt="" width={"500px"} className="" />
-        <img src={territory} alt="" width={"450px"} className="" />
+      <div className="w-full md:w-1/2 h-64 md:h-50 relative">
+        <img
+          src={Tracker}
+          alt="Ford F-150"
+          className="absolute w-[60%] bottom-0 left-[5%] md:left-[5] z-10 transform scale-x-[-1]"
+        />
+
+        <img
+          src={territory}
+          alt="Ford Territory"
+          className="absolute w-[55%] bottom-0 right-[5%] z-20 transform scale-x-[-1]"
+        />
       </div>
-    </div>
+    </section>
   );
 }

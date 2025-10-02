258038e8506fe317388ae483d49b7c6122ae7204
Laureano <laureanojuarez69@gmail.com>
2025-10-02 18:56:06 -0300

    feat: Implement user authentication with registration and login functionality, including validation and password hashing

--- integrador-backend/backend/src/index.js
+++ integrador-backend/backend/src/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import {sequelize} from "./db.js";
+import "./models/User.js";
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+try {
+  await sequelize.sync();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
--- integrador-backend/backend/src/models/User.js
+++ integrador-backend/backend/src/models/User.js
@@ -1,8 +1,29 @@
-import { sequelize } from "../db";
-import { DataTypes } from "sequelize";
+import {sequelize} from "../db.js";
+import {DataTypes} from "sequelize";
 
-const User = sequelize.define("user", {
-  username: {
-    type: DataTypes.STRING,
+export const User = sequelize.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-});
+  {
+    timestamps: false,
+  }
+);
